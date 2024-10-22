5bed47c986b2f21b35289f372f2730323c8faed8
Kanwal Khalid <121491411+kanwalkhd@users.noreply.github.com>
2024-10-22 17:24:43 +0500

    Create models for app

--- todo_app/admin.py
+++ todo_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+
+class ToDoAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in ToDo._meta.fields if field.name != 'user']
+
+
+admin.site.register(ToDo)
--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class ToDo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='todos')
+    title = models.CharField(max_length=100)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
